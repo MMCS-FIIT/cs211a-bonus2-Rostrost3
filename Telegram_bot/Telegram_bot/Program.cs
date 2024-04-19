4566f1b676b63f55f089d8a14bd7f896136bbe23
Ростислав Макайда <rostikmakayda@outlook.com>
2024-04-19 20:30:17 +0300

    Added so that he would take a random football player

--- Telegram_bot/Telegram_bot/Program.cs
+++ Telegram_bot/Telegram_bot/Program.cs
@@ -9,21 +9,28 @@ namespace Telegram_bot
 {
     internal class Program
     {
+        public static Random r = new Random();
+
         public static int c,i = 0; //баллы игрока,индекс правильного ответа для определённого вопроса
-        public static Dictionary<string, string[]> FootballPlayers;
+
+        public static Dictionary<int, string[]> FootballPlayers; //int для того, чтобы вопросы выпадали рандомно. Массив состоит из фото и вариантов
+
         public static List<string> CorrectAnswers;
+
+        public static List<int> ListOfRandoms; //Чтобы вопросы не повторялись, потому что я кидаю их рандомно1
 
         static void Main(string[] args)
         {
-            FootballPlayers = new Dictionary<string, string[]>();
-            FootballPlayers["https://s-cdn.sportbox.ru/images/styles/upload/fp_fotos/bd/b0/95dd9d6c42d283d22a6ef81a48f850d2657879e3a203e325196686.jpg"] = new string[] { "Cristiano Ronaldo,Al-Nassr", "Lionel Messi,Inter Miami" };
-            FootballPlayers["https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fd060a46a-97ad-48ba-9299-adce61729dc8.jpg?crop=5000%2C2812%2C0%2C58&resize=1200"] = new string[] { "Jude Bellingham,Real Madrid", "Erling Haaland,Manchester City", "Rodri,Manchester City", "Cristiano Ronaldo,Al-Nassr" };
-            FootballPlayers["https://cdn.iz.ru/sites/default/files/styles/900x506/public/news-2019-08/2019-08-29T173343Z_1926237047_RC1615819390_RTRMADP_3_SOCCER-CHAMPIONS-DRAW.jpg?itok=ravgNCj1"] = new string[] { "Erling Haaland,Manchester City", "Virgil van Dijk", "Vinícius Júnior", "Karim Benzema" };
-            //Сделать рандомно, то есть ключ - цифра и цифру берём рандомно, а затем запоминаем её во множестве, чтобы не повторялось
+            FootballPlayers = new Dictionary<int, string[]>();
+            FootballPlayers[0] = new string[] { "https://s-cdn.sportbox.ru/images/styles/upload/fp_fotos/bd/b0/95dd9d6c42d283d22a6ef81a48f850d2657879e3a203e325196686.jpg","Cristiano Ronaldo", "Lionel Messi" };
+            FootballPlayers[1] = new string[] { "https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fd060a46a-97ad-48ba-9299-adce61729dc8.jpg?crop=5000%2C2812%2C0%2C58&resize=1200","Jude Bellingham", "Erling Haaland", "Rodri", "Cristiano Ronaldo" };
+            FootballPlayers[2] = new string[] { "https://cdn.iz.ru/sites/default/files/styles/900x506/public/news-2019-08/2019-08-29T173343Z_1926237047_RC1615819390_RTRMADP_3_SOCCER-CHAMPIONS-DRAW.jpg?itok=ravgNCj1","Erling Haaland", "Virgil van Dijk", "Vinícius Júnior", "Karim Benzema" };
+            
             CorrectAnswers = new List<string>();
-            CorrectAnswers.Add("Cristiano Ronaldo,Al-Nassr");
-            CorrectAnswers.Add("Erling Haaland,Manchester City");
+            CorrectAnswers.Add("Cristiano Ronaldo");
+            CorrectAnswers.Add("Erling Haaland");
             CorrectAnswers.Add("Virgil van Dijk");
+
             var client = new TelegramBotClient("6668334841:AAFfb4xkbEGL-HQ_d7AEdABDL2JgH30CH7Y");
             client.StartReceiving(Update, Error);
             Console.ReadLine();
@@ -34,7 +41,8 @@ namespace Telegram_bot
             try
             {
                 List<string> AnswId = new List<string>();
-                switch(update.Type)
+                List<int> ListOfRandoms = new List<int>();
+                switch (update.Type)
                 {
                     case UpdateType.Message:
                     {
@@ -56,36 +64,46 @@ namespace Telegram_bot
                                     }
                                     if (message.Text.ToLower().Contains("играть") || message.Text.ToLower().Contains("/play"))
                                     {
-                                            i = 0;
+                                            AnswId.Add(message.MessageId.ToString());
                                             c = 0; //новая игра => баллы обнуляются
                                             await botclient.SendTextMessageAsync(message.Chat.Id, "Давай поиграем!\nТебе нужно будет угадать игрока\nЗа каждый правильный ответ 1 балл\nУдачи!");
                                             Thread.Sleep(2000);
 
-                                            foreach (var x in FootballPlayers)
+                                            while(ListOfRandoms.Count() != FootballPlayers.Count())
                                             {
-                                                await botclient.SendPhotoAsync(message.Chat.Id, InputFile.FromUri(x.Key));
+                                                int y = r.Next(0, FootballPlayers.Count());
+                                                while(ListOfRandoms.Contains(y))
+                                                {
+                                                    y = r.Next(0, FootballPlayers.Count());
+                                                }
+                                                ListOfRandoms.Add(y);
+
+                                                await botclient.SendPhotoAsync(message.Chat.Id, InputFile.FromUri(FootballPlayers[y][0]));
                                                 var a = new List<InlineKeyboardButton[]>();
                                                 var inlineKeyboard1 = new InlineKeyboardMarkup(a);
-                                                for(int i = 0; i < x.Value.Length; i++)
+                                                for(int i = 1; i < FootballPlayers[y].Length; i++)
                                                 {
-                                                    a.Add(new InlineKeyboardButton[] { FootballPlayers[x.Key][i] });
+                                                    a.Add(new InlineKeyboardButton[] { FootballPlayers[y][i] });
                                                 }
-                                                await botclient.SendTextMessageAsync(message.Chat.Id, "Кто же это?", replyMarkup: inlineKeyboard1);
+                                                var b = await botclient.SendTextMessageAsync(message.Chat.Id, "Кто же это?", replyMarkup: inlineKeyboard1);
 
                                                 // Ожидание ответа от пользователя
                                                 Update selectedUpdate = null;
                                                 while (selectedUpdate == null)
                                                 {
-                                                    //Сделать так, чтобы работали тут команды выше, всё из-за фореача?
                                                     var updates = await botclient.GetUpdatesAsync();
                                                     selectedUpdate = updates.FirstOrDefault(u => u.Type == UpdateType.CallbackQuery && u.CallbackQuery.Message.Chat.Id == message.Chat.Id && !AnswId.Contains(u.CallbackQuery.Id.ToString())); //Чтобы он не брал предыдущий ответ
+                                                    if (updates.FirstOrDefault(u => u.Type == UpdateType.Message && !AnswId.Contains(u.Message.MessageId.ToString())) != null)
+                                                    {
+                                                        return;
+                                                    }
                                                     // Пауза перед повторной проверкой
                                                     await Task.Delay(500);
                                                 }
 
                                                 // Обработка выбранного ответа
                                                 var selectedPlayer = selectedUpdate.CallbackQuery.Data;
-                                                if (CorrectAnswers[i] == selectedPlayer)
+                                                if (CorrectAnswers[y] == selectedPlayer)
                                                 {
                                                     c++;
                                                     await botclient.SendTextMessageAsync(message.Chat.Id, $"Поздравляю!\n{selectedPlayer} - правильный ответ!\nУ вас {c} балл(ов)");
@@ -95,7 +113,6 @@ namespace Telegram_bot
                                                     await botclient.SendTextMessageAsync(message.Chat.Id, $"Не расстраивайтесь\n{selectedPlayer} - неправильный ответ!\nУ вас {c} балл(ов)");
                                                 }
                                                 AnswId.Add(selectedUpdate.CallbackQuery.Id.ToString());
-                                                i++;
                                             }
                                             return;
                                     }
